089b9721960805faa8aa86e9e6c2d374e4cb32df
Julio Cesar Dossi Biasutti <dossifutebol@gmail.com>
2026-06-15 00:04:02 -0300

    main

--- ap.js
+++ ap.js
@@ -3,16 +3,7 @@ const btnExportar = document.getElementById("btnExportar");
 const outJson = document.getElementById("outJson");
 
 const produtos = [
-    {
-        nome: "Jogo PRAGMATA - PS5",
-        preco: 299.00,
-        estilo: "Ação e Aventura",
-    },
-    {
-        nome: "Jogo Life is Strange Reunion - PS5",
-        preco: 299.00,
-        estilo: "Narrativo",
-    },
+
     {
         nome: "Jogo Hades II - PS5",
         preco: 69.00,
@@ -24,14 +15,29 @@ const produtos = [
         estilo: "FPS",
     },
     {
+        nome: "Jogo PRAGMATA - PS5",
+        preco: 299.00,
+        estilo: "Ação e Aventura",
+    },
+    {
+        nome: "Jogo Life is Strange Reunion - PS5",
+        preco: 299.00,
+        estilo: "Narrativo",
+    },
+    {
         nome: "Wolverine (pré-venda) - PS5",
         preco: 399.00,
         estilo: "Ação e Aventura ",
+    },
+    {
+        nome: "Grand Theft Auto VI (pré-venda) - PS5",
+        preco: 459.00,
+        estilo: "Ação e Aventura",
     }
 
 ]
 
-function criarCard (produto) {
+function criarCard(produto) {
     return `
     <div class = "card">
         <h2>${produto.nome}</h2>
@@ -41,11 +47,11 @@ function criarCard (produto) {
     `
 }
 
-function mostrarproduto () {
+function mostrarproduto() {
     catalogo.innerHTML = ""
 
     for (let renderizar = 0; renderizar < produtos.length; renderizar++) {
-    catalogo.innerHTML += criarCard(produtos[renderizar])
+        catalogo.innerHTML += criarCard(produtos[renderizar])
     }
 }
 
